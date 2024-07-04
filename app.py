2e56cb94b6f99c5dafc1acb56e29253defaa2994
Victorasuquo <jacobasuquo199@email.com>
2024-07-04 13:00:39 +0100

    coolest

--- app.py
+++ app.py
@@ -235,4 +235,16 @@ def video():
 def webapp():
     return Response(generate_frames_web(path_x=0), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
